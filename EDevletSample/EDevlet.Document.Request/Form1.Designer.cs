234698e73454721d2a3ca1d24bbda7e7b6840964
zeljkosamed <smdkthyli@gmail.com>
2022-07-04 15:40:19 +0300

    Add project files.

---
+++ EDevletSample/EDevlet.Document.Request/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace EDevlet.Document.Request
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnCreateDocument = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Connection String";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(245, 83);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(289, 22);
+            this.txtConnectionString.TabIndex = 2;
+            this.txtConnectionString.Text = "amqp://guest:guest@localhost:5672";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(562, 71);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(122, 47);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnCreateDocument
+            // 
+            this.btnCreateDocument.Location = new System.Drawing.Point(288, 222);
+            this.btnCreateDocument.Name = "btnCreateDocument";
+            this.btnCreateDocument.Size = new System.Drawing.Size(158, 62);
+            this.btnCreateDocument.TabIndex = 4;
+            this.btnCreateDocument.Text = "Create Document";
+            this.btnCreateDocument.UseVisualStyleBackColor = true;
+            this.btnCreateDocument.Click += new System.EventHandler(this.btnCreateDocument_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(45, 383);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(677, 292);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 687);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCreateDocument);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnCreateDocument;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ EDevletSample/EDevlet.Document.Request/Form1.cs
@@ -0,0 +1,93 @@
+﻿using EDevlet.Document.Common;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDevlet.Document.Request
+{
+    public partial class Form1 : Form
+    {
+        IConnection connection;
+
+        private readonly string createDocument = "create_document_queue";
+        private readonly string documentCreated = "document_created_queue";
+        private readonly string documentCreateExchange = "document_create_exchange";
+
+        IModel _channel;
+        IModel channel => _channel ?? (_channel = GetChannel());
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (connection == null || !connection.IsOpen)
+                connection = GetConnection();
+
+            btnCreateDocument.Enabled = true;
+
+            channel.ExchangeDeclare(documentCreateExchange, "direct");
+
+            channel.QueueDeclare(createDocument, false, false, false);
+            channel.QueueBind(createDocument, documentCreateExchange, createDocument);
+
+            channel.QueueDeclare(documentCreated, false, false, false);
+            channel.QueueBind(documentCreated, documentCreateExchange, documentCreated);
+        }
+
+        private void btnCreateDocument_Click(object sender, EventArgs e)
+        {
+            var model = new CreateDocumentModel()
+            {
+                UserId = 1,
+                DocumentType = DocumentType.Pdf
+            };
+
+            WriteToQueue(createDocument, model);
+
+            var consumerEvent = new EventingBasicConsumer(channel);
+            consumerEvent.Received += (ch, ea) =>
+            {
+                var modelReceived = JsonConvert.DeserializeObject<CreateDocumentModel>(Encoding.UTF8.GetString(ea.Body.ToArray()));
+            };
+
+            channel.BasicConsume(documentCreated, true, consumerEvent);
+
+        }
+
+
+        private void WriteToQueue(string queueName, CreateDocumentModel model)
+        {
+            var messageArr = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model));
+
+            channel.BasicPublish(documentCreateExchange, queueName, null, messageArr);
+        }
+
+        private IModel GetChannel()
+        {
+            return connection.CreateModel();
+        }
+
+        private IConnection GetConnection()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(txtConnectionString.Text)
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
